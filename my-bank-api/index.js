d04f829298ccf737fd6c8482752203334166bad7
Rafael Vilas Boas de Carvalho <az.carta@gmail.com>
2021-08-10 11:02:54 -0300

    my-bank-api

--- my-bank-api/index.js
+++ my-bank-api/index.js
@@ -1,28 +1,49 @@
 import express from "express";
+import winston from "winston";
 import accountsRouter from "./routes/accounts.js";
-import {promises as fs} from "fs";
+import { promises as fs } from "fs";
 
-const { readFile, writeFile} = fs;
+const { readFile, writeFile } = fs;
+const { combine, timestamp, label, printf } = winston.format;
+const myFormat = printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+global.fileName = "accounts.json"
+
+global.logger = winston.createLogger({
+    level: "silly",
+    transports: [
+        new (winston.transports.Console)(),
+        new (winston.transports.File)({ filename: "my-bank-api.log" })
+    ],
+    format: combine(
+        label({ label: "my-bank-api" }),
+        timestamp(),
+        myFormat
+    )
+
+})
 
 const app = express();
 app.use(express.json());
-
 app.use("/account", accountsRouter);
+app.use(express.static("public"));
 
-app.listen(8080, async ()=>{
-    try{
-        await readFile("accounts.json");
-        console.log("server online");
-    }catch(err){
+app.listen(8080, async () => {
+    try {
+        await readFile(fileName);
+        logger.info("server online");
+    } catch (err) {
         const initialJson = {
             nextId: 1,
             accounts: []
         }
-        writeFile("accounts.json", JSON.stringify(initialJson)).then(()=>{
-            console.log("server online, file created");
-        }).catch(err =>{
-            console.log(err);
+        writeFile(fileName, JSON.stringify(initialJson)).then(() => {
+            logger.info("server online, file created");
+        }).catch(err => {
+            logger.error(err);
         });
     }
-  
+
 });
--- my-bank-api/routes/accounts.js
+++ my-bank-api/routes/accounts.js
@@ -1,60 +1,134 @@
-import { triggerAsyncId } from "async_hooks";
 import express from "express";
 import { promises as fs } from "fs";
 
-const { readFile, writeFile } = fs;
 
-global.fileName = "accounts.json"
+const { readFile, writeFile } = fs;
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+router.post("/", async (req, res, next) => {
     try {
         let account = req.body;
+
+        if (!account.name || account.balance == null) {
+            throw new Error("Name and Baance are needed");
+        }
+
         const data = JSON.parse(await readFile(fileName));
 
-        account = { id: data.nextId++, ...account };
+        account = {
+            id: data.nextId++,
+            name: account.name,
+            balance: account.balance
+        };
         data.accounts.push(account);
 
         await writeFile(fileName, JSON.stringify(data, null, 2));
 
         res.send(account);
+
+        logger.info(`POST /account - ${JSON.stringify(account)}`)
     } catch (err) {
-        res.status(400).send({ error: err.message });
+        next(err);
     }
 });
 
-router.get("/", async (req, res) => {
+router.get("/", async (req, res, next) => {
     try {
         const data = JSON.parse(await readFile(fileName));
         delete data.nextId;
         res.send(data);
+        logger.info("GET /account");
     } catch (err) {
-        res.status(400).send({ error: err.message });
+        next(err);
     }
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", async (req, res, next) => {
     try {
         const data = JSON.parse(await readFile(fileName));
         const account = data.accounts.find(
             account => account.id == req.params.id);
         res.send(account);
+        logger.info("GET /account/:id");
     } catch (err) {
-        res.status(400).send({ error: err.message });
+        next(err);
     }
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", async (req, res, next) => {
     try {
         const data = JSON.parse(await readFile(fileName));
         data.accounts = data.accounts.filter(
             account => account.id !== parseInt(req.params.id));
         await writeFile(fileName, JSON.stringify(data, null, 2));
         res.end();
+        logger.info(`DELETE /account/:id - ${req.params.id}`);
     } catch (err) {
-        res.status(400).send({ error: err.message });
+        next(err);
     }
+});
+
+router.put("/", async (req, res, next) => {
+    try {
+        const account = req.body;
+
+        if (!account.id || !account.name || account.balance == null) {
+            throw new Error("ID, Name and Baance are needed");
+        }
+
+        const data = JSON.parse(await readFile(fileName));
+        const index = data.accounts.findIndex(
+            a => a.id === account.id
+        );
+
+        if (index === -1) {
+            throw new Error("Data not found");
+        }
+
+        data.accounts[index] = account;
+
+        await writeFile(fileName, JSON.stringify(data));
+
+        res.send(account);
+        logger.info(`PUT /account - ${JSON.stringify(account)}`)
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.patch("/updateBalance", async (req, res, next) => {
+    try {
+        const account = req.body;
+
+        if (!account.id || account.balance == null) {
+            throw new Error("ID and Baance are needed");
+        }
+
+        const data = JSON.parse(await readFile(fileName));
+        const index = data.accounts.findIndex(
+            a => a.id === account.id
+        );
+
+        if (index === -1) {
+            throw new Error("Data not found");
+        }
+
+        data.accounts[index].balance = account.balance;
+
+        await writeFile(fileName, JSON.stringify(data));
+
+        res.send(data.accounts[index]);
+        logger.info(`PATCH /account/updateBalance - ${JSON.stringify(account)}`)
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.use((err, req, res, next) => {
+    logger.error(`${req.method} ${req.baseUrl} ${err.message}`);
+    console.log(err);
+    res.status(400).send({ error: err.message });
 });
 
 export default router;
